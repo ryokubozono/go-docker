9d657ecae552dfdad18263996f6c8e63beefa185
ryokubozono <snusmumrik72@gmail.com>
2021-11-03 11:36:58 +0900

    refactor: dbpingをhandlerに分割

--- src/handler/rootFunc.go
+++ src/handler/rootFunc.go
@@ -2,14 +2,51 @@ package handler
 
 import (
 	"net/http"
-	
+    "fmt"
+    "database/sql"
+    _ "github.com/go-sql-driver/mysql"
+
 	"github.com/gin-gonic/gin"
 )
+
+type User struct {
+    ID   int
+    Name string
+}
 
 func RootGet() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
             "message": "hello world",
+        })
+	}
+}
+
+func DbPing() gin.HandlerFunc{
+	return func(c *gin.Context){
+        db, err := sql.Open("mysql", "root:password@tcp(mysql:3306)/test_db")
+        defer db.Close()
+        if err != nil {
+            fmt.Println(err.Error())
+        }
+
+        rows, err := db.Query("SELECT * FROM test_table")
+        defer rows.Close()
+        if err != nil {
+            fmt.Println(err)
+        }
+
+        user := User{}
+        for rows.Next() {
+            err = rows.Scan(&user.ID, &user.Name)
+            if err != nil {
+                fmt.Println(err)
+            }
+            fmt.Println(user)
+        }
+
+        c.JSON(200, gin.H{
+            "hello": user.Name,
         })
 	}
 }
--- src/main.go
+++ src/main.go
@@ -2,46 +2,16 @@ package main
 
 import (
     "github.com/gin-gonic/gin"
-    "fmt"
-    "database/sql"
-    _ "github.com/go-sql-driver/mysql"
+
     "github.com/ryokubozono/go-docker/handler"
 )
-
-type User struct {
-    ID   int
-    Name string
-}
 
 func main() {
     engine:= gin.Default()
     engine.GET("/", handler.RootGet())
 
-    engine.GET("/ping", func(c *gin.Context){
-        db, err := sql.Open("mysql", "root:password@tcp(mysql:3306)/test_db")
-        defer db.Close()
-        if err != nil {
-            fmt.Println(err.Error())
-        }
+    engine.GET("/ping", handler.DbPing())
 
-        rows, err := db.Query("SELECT * FROM test_table")
-        defer rows.Close()
-        if err != nil {
-            fmt.Println(err)
-        }
-
-        user := User{}
-        for rows.Next() {
-            err = rows.Scan(&user.ID, &user.Name)
-            if err != nil {
-                fmt.Println(err)
-            }
-            fmt.Println(user)
-        }
-
-        c.JSON(200, gin.H{
-            "hello": user.Name,
-        })
-    })
     engine.Run(":8080")
 }
+
