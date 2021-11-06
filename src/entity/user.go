91480f44cba4dec47d2b8bfc18a411eeda7b881c
ryokubozono <snusmumrik72@gmail.com>
2021-11-06 17:42:55 +0900

    feat: userテーブルを追加

--- src/entity/user.go
+++ src/entity/user.go
@@ -6,4 +6,6 @@ import (
 
 type User struct {
     gorm.Model
+    Username string `form:"username" binding:"required" gorm:"unique;not null"`
+    Password string `form:"password" binding:"required" gorm:"not null"`
 }
